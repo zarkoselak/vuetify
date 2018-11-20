d55793b714e95411791d0c4bf4d0f7be29571ad5
Andrew Henry <andrew_henry@fcp.biz>
2018-11-20 14:12:55 -0500

    feat(VDualList): missing file

---
+++ packages/vuetify/src/components/VDualList/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-spacer />
+      <v-flex xs4>
+        <v-dual-list
+          :leftListData="leftListData"
+          :rightListData="rightListData"
+          sort="asc"
+          sortKey="id"
+          dataKey="id"
+          textKey="text"
+        ></v-dual-list>
+      </v-flex>
+      <v-spacer />
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import VDualList from './VDualList';
+  export default {
+    components: {
+      VDualList,
+    },
+    data: () => ({
+      leftListData: [
+        {id: 1, text: 'Pawn', meta: 'item1' },
+        {id: 2, text: 'Knight', meta: 'item2' },
+        {id: 3, text: 'Rook', meta: 'item3' },
+        {id: 4, text: 'Bishop', meta: 'item4' },
+        {id: 5, text: 'Queen', meta: 'item5' },
+        {id: 6, text: 'King', meta: 'item6' },
+      ],
+      rightListData: [],
+    })
+  }
+</script>
